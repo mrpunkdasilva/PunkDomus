72b8b1c37f77433c2f8c9cc9f87acd05d6add935
Mr. Punk Da Silva <gustavojs417@gmail.com>
2024-09-10 00:15:19 -0300

    init project

---
+++ components/HeaderMain.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <div class="title">
+      <h1>Punk Domus</h1>
+    </div>
+
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/recents">Recentes</NuxtLink>
+      <NuxtLink to="/about">Sobre</NuxtLink>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <HeaderMain/>
+    <Nuxt/>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  font-family: sans-serif;
+  color: #efefef;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  color: #21DEEA;
+}
+
+body {
+  background-color: #1E1E1E;
+}
+
+a {
+  text-decoration: none;
+  color: #fb4885;
+}
+
+p {
+  margin-bottom: 15px;
+}
+
+p:last-of-type {
+  margin-bottom: 0px;
+}
+</style>
