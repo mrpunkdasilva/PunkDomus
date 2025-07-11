860c66b45be8eb0854e16fb45eb097b3c0c6a79f
Mr Punk da Silva <gustavojs417@gmail.com>
2025-07-11 14:10:05 -0300

    add og images

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,6 @@ export default {
       { name: 'twitter:creator', content: '@mrpunksama' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'icon', type: 'image/svg+xml', href: '/punk_domus.svg' },
       {
         rel: 'stylesheet',
